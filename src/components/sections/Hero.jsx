aad9618e0cad652353d53de1cedfb704270228cc
AshmitPaul-code <ashmitpaul369@gmail.com>
2026-09-16 17:36:50 +0530

    fix(hero): extend portrait height upwards to level with top tagline/Ashmit Paul heading and cover project cards

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -152,7 +152,7 @@ export default function Hero() {
             </div>
           </div>
 
-          <div style={{ position: "relative", display: "flex", justifyContent: "center", pointerEvents: "none", marginTop: isMobile ? 0 : 38 }}>
+          <div style={{ position: "relative", display: "flex", justifyContent: "center", pointerEvents: "none", marginTop: isMobile ? 0 : -45 }}>
             <img
               src="./ashmit.png"
               alt="Ashmit Paul"
@@ -162,12 +162,12 @@ export default function Hero() {
               decoding="async"
               style={{
                 width: "100%",
-                maxWidth: 365,
+                maxWidth: 415,
                 borderRadius: 24,
                 userSelect: "none",
                 pointerEvents: "none",
-                WebkitMaskImage: "linear-gradient(to bottom, black 85%, transparent 100%)",
-                maskImage: "linear-gradient(to bottom, black 85%, transparent 100%)",
+                WebkitMaskImage: "linear-gradient(to bottom, black 82%, transparent 100%)",
+                maskImage: "linear-gradient(to bottom, black 82%, transparent 100%)",
               }}
             />
           </div>
